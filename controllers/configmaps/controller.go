509de4488c63a0da3dbea6b54de60c180a51e5a9
C Shi <icydoge@gmail.com>
2020-06-01 16:53:24 +0100

    Stop using buffer, instead to use controller loop

--- controllers/configmaps/controller.go
+++ controllers/configmaps/controller.go
@@ -4,15 +4,11 @@ import (
 	"log"
 	"time"
 
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	corelisters "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
-
-	"github.com/icydoge/Order/controllers/buffer"
 )
 
 // ConfigMapsController is a controller monitoring changes to ConfigMaps
@@ -31,12 +27,11 @@ func NewConfigMapsController(clientSet kubernetes.Interface, resyncInterval time
 		factory: informerFactory,
 	}
 
-	// We don't process delete, as unintentional deletion of a mounted configmap coupled
-	// with an immediate restart will make things fall over, which is usually not
-	// desirable.
+	// We don't process informer events for the time being, and just rely on
+	// fixed interval resynchronizations.
 	informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    controller.add,
-		UpdateFunc: controller.update,
+		AddFunc:    func(obj interface{}) {},
+		UpdateFunc: func(old interface{}, new interface{}) {},
 		DeleteFunc: func(obj interface{}) {},
 	})
 
@@ -59,74 +54,5 @@ func (c *ConfigMapsController) Run(stopChan chan struct{}) {
 		log.Fatalln("Failed to wait for cache synchronization")
 	}
 
-	// Push all secrets into buffer to be processed if pod controllers
-	// requiring them are not up-to-date.
-	configMaps, err := c.lister.List(labels.Everything())
-	if err != nil {
-		log.Fatalln("Failed to load secrets initially")
-	}
-
-	for _, configMap := range configMaps {
-		item := buffer.BufferItem{
-			TypeMeta:               configMap.TypeMeta,
-			Name:                   configMap.GetName(),
-			Namespace:              configMap.GetNamespace(),
-			PendingResourceVersion: configMap.GetResourceVersion(),
-			LastProcessed:          time.Now(),
-			Attempts:               0,
-		}
-		buffer.Push(&item)
-	}
-
 	<-stopChan
-}
-
-func (c *ConfigMapsController) add(obj interface{}) {
-	configMapState, ok := obj.(*corev1.ConfigMap)
-	if !ok {
-		log.Printf("Could not process add: unexpected type for ConfigMap: %v", obj)
-		return
-	}
-
-	if configMapState == nil {
-		return
-	}
-
-	item := buffer.BufferItem{
-		TypeMeta:               configMapState.TypeMeta,
-		Name:                   configMapState.GetName(),
-		Namespace:              configMapState.GetNamespace(),
-		PendingResourceVersion: configMapState.GetResourceVersion(),
-		LastProcessed:          time.Now(),
-		Attempts:               0,
-	}
-	buffer.Push(&item)
-}
-
-func (c *ConfigMapsController) update(old, new interface{}) {
-	oldState, ok := old.(*corev1.ConfigMap)
-	if !ok {
-		log.Printf("Could not process update: unexpected old state type for ConfigMap: %v", new)
-		return
-	}
-	newState, ok := new.(*corev1.ConfigMap)
-	if !ok {
-		log.Printf("Could not process update: unexpected new state type for ConfigMap: %v", new)
-		return
-	}
-
-	if newState.GetResourceVersion() == oldState.GetResourceVersion() {
-		// No change in configmap
-		return
-	}
-
-	item := buffer.BufferItem{
-		TypeMeta:               newState.TypeMeta,
-		Name:                   newState.GetName(),
-		Namespace:              newState.GetNamespace(),
-		PendingResourceVersion: newState.GetResourceVersion(),
-		LastProcessed:          time.Now(),
-		Attempts:               0,
-	}
-	buffer.Push(&item)
 }
--- controllers/secrets/controller.go
+++ controllers/secrets/controller.go
@@ -4,15 +4,11 @@ import (
 	"log"
 	"time"
 
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	corelisters "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
-
-	"github.com/icydoge/Order/controllers/buffer"
 )
 
 // SecretsController is a controller monitoring changes to secrets
@@ -31,12 +27,11 @@ func NewSecretsController(clientSet kubernetes.Interface, resyncInterval time.Du
 		factory: informerFactory,
 	}
 
-	// We don't process delete, as unintentional deletion of a mounted secret coupled
-	// with an immediate restart will make things fall over, which is usually not
-	// desirable.
+	// We don't process informer events for the time being, and just rely on
+	// fixed interval resynchronizations.
 	informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc:    controller.add,
-		UpdateFunc: controller.update,
+		AddFunc:    func(obj interface{}) {},
+		UpdateFunc: func(old interface{}, new interface{}) {},
 		DeleteFunc: func(obj interface{}) {},
 	})
 
@@ -59,70 +54,5 @@ func (c *SecretsController) Run(stopChan chan struct{}) {
 		log.Fatalln("Failed to wait for cache synchronization")
 	}
 
-	// Push all secrets into buffer to be processed if pod controllers
-	// requiring them are not up-to-date.
-	secrets, err := c.lister.List(labels.Everything())
-	if err != nil {
-		log.Fatalln("Failed to load secrets initially")
-	}
-
-	for _, secret := range secrets {
-		item := buffer.BufferItem{
-			TypeMeta:               secret.TypeMeta,
-			Name:                   secret.GetName(),
-			Namespace:              secret.GetNamespace(),
-			PendingResourceVersion: secret.GetResourceVersion(),
-			LastProcessed:          time.Now(),
-			Attempts:               0,
-		}
-		buffer.Push(&item)
-	}
-
 	<-stopChan
-}
-
-func (c *SecretsController) add(obj interface{}) {
-	secretState, ok := obj.(*corev1.Secret)
-	if !ok {
-		log.Printf("Could not process add: unexpected type for Secret: %v", obj)
-		return
-	}
-
-	item := buffer.BufferItem{
-		TypeMeta:               secretState.TypeMeta,
-		Name:                   secretState.GetName(),
-		Namespace:              secretState.GetNamespace(),
-		PendingResourceVersion: secretState.GetResourceVersion(),
-		LastProcessed:          time.Now(),
-		Attempts:               0,
-	}
-	buffer.Push(&item)
-}
-
-func (c *SecretsController) update(old, new interface{}) {
-	oldState, ok := old.(*corev1.Secret)
-	if !ok {
-		log.Printf("Could not process update: unexpected old state type for Secret: %v", new)
-		return
-	}
-	newState, ok := new.(*corev1.Secret)
-	if !ok {
-		log.Printf("Could not process update: unexpected new state type for Secret: %v", new)
-		return
-	}
-
-	if newState.GetResourceVersion() == oldState.GetResourceVersion() {
-		// No change in secret
-		return
-	}
-
-	item := buffer.BufferItem{
-		TypeMeta:               newState.TypeMeta,
-		Name:                   newState.GetName(),
-		Namespace:              newState.GetNamespace(),
-		PendingResourceVersion: newState.GetResourceVersion(),
-		LastProcessed:          time.Now(),
-		Attempts:               0,
-	}
-	buffer.Push(&item)
 }
